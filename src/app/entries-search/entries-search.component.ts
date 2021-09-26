249419808a7a97f2eb1c2d2b2f00915c84aec6a5
Rafhael <rafhael.dev@gmail.com>
2021-09-26 07:10:22 -0300

    Added some pipes for columns table

--- src/app/entries-search/entries-search.component.ts
+++ src/app/entries-search/entries-search.component.ts
@@ -8,25 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class EntriesSearchComponent {
 
   entries = [
-    { entryType: 'EXPENSE', description: 'Kufathir', dueDate: '30/06/2021',
+    { entryType: 'EXPENSE', description: 'Kufathir', dueDate: new Date(2021, 6, 30),
       paymentDate: null, price: 4.55, person: 'Eazkioli' },
 
-    { entryType: 'EARNING', description: 'Iszoion', dueDate: '10/06/2021',
-      paymentDate: '09/06/2021', price: 80000, person: 'Nirond' },
+    { entryType: 'EARNING', description: 'Iszoion', dueDate: new Date(2021, 6, 10),
+      paymentDate: new Date(2021, 6, 9), price: 80000, person: 'Nirond' },
 
-    { entryType: 'EXPENSE', description: 'Keinnoa', dueDate: '20/07/2021',
+    { entryType: 'EXPENSE', description: 'Keinnoa', dueDate: new Date(2021, 7, 20),
       paymentDate: null, price: 14312, person: 'Orgur' },
 
-    { entryType: 'EXPENSE', description: 'Baga', dueDate: '05/06/2021',
-      paymentDate: '30/05/2021', price: 800, person: 'Teasi' },
+    { entryType: 'EXPENSE', description: 'Baga', dueDate: new Date(2021, 6, 5),
+      paymentDate: new Date(2021, 6, 30), price: 800, person: 'Teasi' },
 
-    { entryType: 'EARNING', description: 'Fewaomub', dueDate: '18/08/2021',
+    { entryType: 'EARNING', description: 'Fewaomub', dueDate: new Date(2021, 8, 18),
       paymentDate: null, price: 55000, person: 'Fewaomub' },
 
-    { entryType: 'EXPENSE', description: 'Mosilo', dueDate: '10/07/2021',
-      paymentDate: '09/07/2021', price: 1750, person: 'Byun' },
+    { entryType: 'EXPENSE', description: 'Mosilo', dueDate: new Date(2021, 7, 10),
+      paymentDate: new Date(2021, 7, 9), price: 1750, person: 'Byun' },
 
-    { entryType: 'EXPENSE', description: 'Winzae', dueDate: '13/07/2021',
+    { entryType: 'EXPENSE', description: 'Winzae', dueDate: new Date(2021, 7, 13),
       paymentDate: null, price: 180, person: 'Pinpayndir' }
   ];
 
