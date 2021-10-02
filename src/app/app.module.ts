c18b5b1d55468ef1a031c931e71aabc86928c818
Rafhael <rafhael.dev@gmail.com>
2021-10-02 07:39:58 -0300

    Started to break persons in a feature module
    ead of 'origin/main' by 2 commits.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { EntriesModule } from './entries/entries.module';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,36 +5,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-// PrimeNG modules
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { TooltipModule } from 'primeng/tooltip';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { CalendarModule } from 'primeng/calendar';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputMaskModule } from 'primeng/inputmask';
-
-// Other Modules
-import { CurrencyMaskModule } from 'ng2-currency-mask';
-
 import { NavbarComponent } from './navbar/navbar.component';
-
-import { PersonsSearchComponent } from './persons-search/persons-search.component';
-import { PersonsNewComponent } from './persons-new/persons-new.component';
-import { PersonsGridComponent } from './persons-grid/persons-grid.component';
 import { MessageComponent } from './message/message.component';
+import { PersonsModule } from './persons/persons.module';
+import { EntriesModule } from './entries/entries.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-
-    PersonsSearchComponent,
-    PersonsNewComponent,
-    PersonsGridComponent,
     MessageComponent
   ],
   imports: [
@@ -45,17 +23,7 @@ import { MessageComponent } from './message/message.component';
     AppRoutingModule,
 
     EntriesModule,
-
-    InputTextModule,
-    ButtonModule,
-    TableModule,
-    TooltipModule,
-    InputTextareaModule,
-    CalendarModule,
-    SelectButtonModule,
-    DropdownModule,
-    InputMaskModule,
-    CurrencyMaskModule
+    PersonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/persons/persons.module.ts
@@ -0,0 +1,35 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
+import { InputMaskModule } from 'primeng/inputmask';
+
+import { PersonsSearchComponent } from './persons-search/persons-search.component';
+import { PersonsNewComponent } from './persons-new/persons-new.component';
+import { PersonsGridComponent } from './persons-grid/persons-grid.component';
+
+@NgModule({
+  declarations: [
+    PersonsSearchComponent,
+    PersonsNewComponent,
+    PersonsGridComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    InputMaskModule
+  ],
+  exports: [
+    PersonsSearchComponent,
+    PersonsNewComponent,
+  ]
+})
+export class PersonsModule { }
