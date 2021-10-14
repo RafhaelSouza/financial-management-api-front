9ab25e2546c7aec5ea7e153cd6e547232d687086
Rafhael <rafhael.dev@gmail.com>
2021-10-14 06:58:15 -0300

    Added error handler for http errors on range 400

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+import { HttpErrorResponse } from '@angular/common/http';
+
 import { MessageService } from 'primeng/api';
 
 @Injectable()
@@ -12,6 +14,17 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+    }
+    else if (errorResponse instanceof HttpErrorResponse
+        && errorResponse.status >= 400 && errorResponse.status <= 499) {
+      msg = 'There was an error processing your request.';
+
+      try {
+        msg = errorResponse.error[0].user_message;
+      } catch (e) { }
+
+      console.error('An error has occurred', errorResponse);
+
     } else {
       msg = 'Error processing remote service. Try again.';
       console.error('An error has occurred', errorResponse);
