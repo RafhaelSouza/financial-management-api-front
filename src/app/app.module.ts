fa7e961e0d2ad3542003214f70e73ce1352ad431
Rafhael <rafhael.dev@gmail.com>
2021-10-14 06:08:04 -0300

    Rearranging imports and declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,13 @@
-import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { registerLocaleData } from '@angular/common';
-import localePt from '@angular/common/locales/pt';
-
-import { MessageService, ConfirmationService } from 'primeng/api';
-import { ToastModule } from 'primeng/toast';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { PersonsModule } from './persons/persons.module';
 import { EntriesModule } from './entries/entries.module';
-import { EntryService } from './entries/entry.service';
-import { PersonService } from './persons/person.service';
-
-registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -27,24 +16,14 @@ registerLocaleData(localePt);
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule,
     AppRoutingModule,
     HttpClientModule,
-
-    ToastModule,
-    ConfirmDialogModule,
 
     CoreModule,
     EntriesModule,
     PersonsModule
   ],
-  providers: [
-    { provide: LOCALE_ID, useValue: 'pt-BR' },
-    MessageService,
-    ConfirmationService,
-    EntryService,
-    PersonService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,39 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+
+import { EntryService } from './../entries/entry.service';
+import { PersonService } from './../persons/person.service';
 import { NavbarComponent } from './navbar/navbar.component';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+
+    ToastModule,
+    ConfirmDialogModule,
   ],
-  exports: [NavbarComponent]
+  exports: [
+    NavbarComponent,
+    ToastModule,
+    ConfirmDialogModule
+  ],
+  providers: [
+    EntryService,
+    PersonService,
+    //ErrorHandlerService,
+
+    ConfirmationService,
+    MessageService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ]
 })
 export class CoreModule { }
