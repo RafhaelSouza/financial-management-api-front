b38b615aae793c615afa628473267e73400c6f51
Rafhael <rafhael.dev@gmail.com>
2021-10-15 06:36:52 -0300

    Added dynamic loading for persons

--- src/app/entries/entries-new/entries-new.component.ts
+++ src/app/entries/entries-new/entries-new.component.ts
@@ -1,3 +1,4 @@
+import { PersonService } from './../../persons/person.service';
 import { Component, OnInit } from '@angular/core';
 
 import { CategoryService } from './../../categories/category.service';
@@ -17,25 +18,31 @@ export class EntriesNewComponent implements OnInit {
 
   categories = [];
 
-  persons = [
-    { label: 'Kufathir Iszoion', value: 1 },
-    { label: 'Keinnoa Orgur', value: 2 },
-    { label: 'Baga Teasi', value: 3 },
-  ];
+  persons = [];
 
   constructor(
     private errorHandler: ErrorHandlerService,
-    private categoryService:CategoryService
+    private categoryService:CategoryService,
+    private personService:PersonService
   ) { }
 
   ngOnInit(): void {
     this.loadCategories();
+    this.loadPersons();
   }
 
   loadCategories() {
     return this.categoryService.listAll()
       .then(categories => {
         this.categories = categories.map(cat => ({ label: cat.name, value: cat.id }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  loadPersons() {
+    return this.personService.listAll()
+      .then(persons => {
+        this.persons = persons.map(p => ({ label: p.name, value: p.id }));
       })
       .catch(erro => this.errorHandler.handle(erro));
   }
