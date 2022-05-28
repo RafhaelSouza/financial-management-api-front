680bff802d003b2b1928d19d6890eb9dd16e4d28
Rafhael <rafhael.dev@gmail.com>
2022-05-27 23:23:52 -0300

    plotting charts with static data

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  pieChartData = {
+    labels: ['Monthly', 'Educations', 'Leisure', 'Unforeseen'],
+    datasets: [
+      {
+        data: [2500, 2700, 550, 235],
+        backgroundColor: ['#FF9900', '#109618', '#990099', '#3B3EAC']
+      }
+    ]
+  };
+  lineChartData = {
+    labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    datasets: [
+      {
+        label: 'Earnings',
+        data: [4, 10, 18, 5, 1, 20, 3],
+        borderColor: '#3366CC'
+      }, {
+        label: 'Expenses',
+        data: [10, 15, 8, 5, 1, 7, 9],
+        borderColor: '#D62B00'
+      }
+    ]
+  };
   constructor() { }
 
   ngOnInit(): void {
